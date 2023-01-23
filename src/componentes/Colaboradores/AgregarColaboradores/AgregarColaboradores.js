c65c8cb4e66787c7ddda34bf6821b053f7322836
anibalcastro <anibalcastro1515@gmail.com>
2023-01-23 10:14:59 -0600

    Agregar - Eliminar Colaboradores

--- src/componentes/Colaboradores/AgregarColaboradores/AgregarColaboradores.js
+++ src/componentes/Colaboradores/AgregarColaboradores/AgregarColaboradores.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AgregarColaboradores.css';
-import {Link} from 'react-router-dom';
+import Swal from 'sweetalert2'
+import { Link } from 'react-router-dom';
 
-function AgregarColaborador() {
+export default function AgregarColaborador() {
 
-    const [idUser, setIdUser] = useState(''); //Id usuario logueado
     const [inputValues, setInputValues] = useState({
-        albumName: '', description: ''
-    }); //album name and description
+        fullName: "",
+        dni: "",
+        telNumber: "",
+        email: "",
+        adress: "",
+        birthday: "",
+        gender: "",
+        bloodType: "",
+        role: "",
+        comments: "",
+    });
 
     //Cualquier cambio, se guarde en el estado
     const handleOnChange = useCallback(event => {
@@ -16,48 +25,226 @@ function AgregarColaborador() {
         setInputValues({ ...inputValues, [name]: value });
     }); // capture inputs
 
-    // function to save Album in DB
-    const guardarCliente = () => {
-        let addAlbum = 
-        {idUser: "62df6b2d29ac0cbc96b15b57", //CAMBIAR POR ID USUARIO
-        albumName: inputValues.albumName,
-        description: inputValues.description}
-        
+
+    const validateInput = () => {
+        let jsonFields = {
+            fullName: "Nombre Completo",
+            dni: "Cedula",
+            telNumber: "Numero de celular",
+            email: "Correo Electronico",
+            adress: "Direccion",
+            birthday: "Fecha de nacimiento",
+            gender: "Genero",
+            bloodType: "Tipo de sangre",
+            comments: "Comentarios",
+        }
+
+        for (const [key, value] of Object.entries(inputValues)) {
+            if (value === "") {
+                for (const [keyField, valueField] of Object.entries(jsonFields)) {
+                    if (keyField === key) {
+                        Swal.fire({
+                            title: 'Error!',
+                            text: 'Debe llenar el campo de ' + valueField,
+                            icon: 'error',
+                            confirmButtonText: 'OK'
+                        })
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
-    
+
+    // function to save Album in DB
+    const guardarColaborador = () => {
+
+        let validate = validateInput();
+
+        if (validate) {
+            let date = new Date(inputValues.birthday);
+            let birthdayEmployee = ((date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear());
+
+            let addNewEmployee = JSON.stringify(
+                {
+                    fullName: inputValues.fullName,
+                    dni: inputValues.dni,
+                    telNumber: inputValues.telNumber,
+                    email: inputValues.email,
+                    password: inputValues.dni,
+                    adress: inputValues.adress,
+                    birthday: birthdayEmployee,
+                    gender: inputValues.gender,
+                    bloodType: inputValues.bloodType,
+                    role: inputValues.role,
+                    comments: inputValues.role,
+                });
+
+            let myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: addNewEmployee,
+                redirect: 'follow'
+            };
+
+            fetch("http://localhost:3014/api/users/create_user", requestOptions)
+                .then(response => response.json())
+                .then(result => {
+                    console.log(result.message);
+                    if (result.message === "Created") {
+                        Swal.fire(
+                            'USUARIO CREADO',
+                            'El usuario ' + inputValues.fullName + ' ha sido creado con exito',
+                            'success'
+                        );
+
+                        window.location.href = '/dashboard/';
+                    }
+                    else {
+                        Swal.fire(
+                            'ERROR',
+                            'Ha ocurrido un error',
+                            'error'
+                        );
+                    }
+                })
+                .catch(error => console.log('error', error));
+
+        }
+    }
+
 
     return (
         <React.Fragment>
+            <div id="divAgregarCliente" className="d-flex justify-content-center ">
+                <div className="wrapper fadeInDown">
+                    <div id="formContent">
+                        <div className="fadeIn first">
+                            <h4 className="title">Agregar nuevo entrenador</h4>
+                        </div>
+                        <form id="formLogin" encType="multipart/form-data">
+                            <input
+                                onChange={handleOnChange}
+                                type="text"
+                                id="fullName"
+                                className="fadeIn second"
+                                name="fullName"
+                                placeholder="NOMBRE COMPLETO"
+                                required
+                            ></input>
 
-        <div className="wrapper fadeInDown">
-            <div id="formContent">
-                <div className="fadeIn first">
-                    <h4 className='title'>Agregar nuevo colaborador</h4>
+                            <input
+                                onChange={handleOnChange}
+                                type="number"
+                                id="dni"
+                                className="fadeIn second"
+                                name="dni"
+                                placeholder="CEDULA"
+                                required
+                            ></input>
+
+                            <input
+                                onChange={handleOnChange}
+                                type="number"
+                                id="telNumber"
+                                className="fadeIn second"
+                                name="telNumber"
+                                placeholder="TELEFONO"
+                                required
+                            ></input>
+
+                            <input
+                                onChange={handleOnChange}
+                                type="text"
+                                id="email"
+                                className="fadeIn second"
+                                name="email"
+                                placeholder="CORREO ELECTRONICO"
+                                required
+                            ></input>
+
+                            <input
+                                onChange={handleOnChange}
+                                type="text"
+                                id="adress"
+                                className="fadeIn second"
+                                name="adress"
+                                placeholder="DIRECCION"
+                                required
+                            ></input>
+                        </form>
+                        <br />
+                    </div>
                 </div>
-                <form id='formLogin'>
-                    <input onChange={handleOnChange} type="text" id="login" className="fadeIn second" name="nombreCompleto" placeholder="NOMBRE COMPLETO" ></input>
 
-                    <input onChange={handleOnChange} type="number" id="login" className="fadeIn second" name="cedula" placeholder="CEDULA" ></input>
+                <div className="wrapper fadeInDown">
+                    <div id="formContent">
+                        <br />
+                        <form id="formLogin">
+                            <input
+                                onChange={handleOnChange}
+                                type="date"
+                                id="birthday"
+                                name="birthday"
+                                required
+                            ></input>
 
-                    <input onChange={handleOnChange} type="number" id="login" className="fadeIn second" name="telefono" placeholder="SALARIO" ></input>
+                            <select onChange={handleOnChange} id="gender" name="gender" required>
+                                <option value="DEFAULT" disabled selected hidden>
+                                    GENERO
+                                </option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                                <option value="Otro">Otro</option>
+                            </select>
 
+                            <select onChange={handleOnChange} id="bloodType" name="bloodType" required>
+                                <option value="" disabled selected hidden>
+                                    TIPO DE SANGRE
+                                </option>
+                                <option value="A">TIPO A</option>
+                                <option value="B">TIPO B</option>
+                                <option value="AB">TIPO AB</option>
+                                <option value="O">TIPO O</option>
+                            </select>
 
-                    <select id="sangre">
-                    <option value="" disabled selected hidden>Asignar Rol</option>
-                        <option value='Masculino'>Administrador</option>
-                        <option value='Femenino'>Entrenador</option>
-                        <option value='Otro'>Recepcionista</option>
-                    </select>
+                            <select
+                                onChange={handleOnChange}
+                                id="role"
+                                name="role"
+                                required
+                            >
+                                <option value="" disabled selected hidden>
+                                    ROL
+                                </option>
+                                <option value="Administrador">Administrador</option>
+                                <option value="Colaborador">Colaborador</option>
+                            </select>
 
-                    <input type="datetime-local" name="cumpleanos"></input>
-        
-                </form>
-                <Link to={'/dashboard/colaboradores'} id="submit" onClick={()=> guardarCliente()} className="btn btn-dark" >Save</Link>
+                            <textarea
+                                onChange={handleOnChange}
+                                rows="5"
+                                cols="100"
+                                placeholder="COMENTARIOS"
+                                name="comments"
+                                required
+                            ></textarea>
+                        </form>
+                        <button
+                            id="submit"
+                            onClick={() => guardarColaborador()}
+                            className="btn btn-dark"
+                        >
+                            Guardar
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
-    </React.Fragment>
+        </React.Fragment>
 
     );
 }
-
-export default AgregarColaborador;
--- src/componentes/Colaboradores/Colaboradores.js
+++ src/componentes/Colaboradores/Colaboradores.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
 
 function Colaboradores() {
 
     let contador = 0;
-    const [clientes, setClientes] = useState([]);
+    const [colaboradores, setColaboradores] = useState([]);
     let array = [
         { id: 1, nombreCompleto: "Gloriana Mendez Espinoza", cedula: "204310766", rol: "Entrenadora", fechaNacimineto: "02/09/1980", salario: "450.000,00" },
         { id: 2, nombreCompleto: "Edgardo Garrote ", cedula: "603030996", rol: "Entrenador", fechaNacimineto: "03/09/1992", salario: "450.000,00" },
@@ -26,17 +27,80 @@ function Colaboradores() {
     }
 
     const llenarLista = () => {
-        setClientes(array);
+        let colaborador = Array();
+
+        let requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost:3014/api/users/employees", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+                colaborador = result.data;
+                setColaboradores(colaborador);
+                //console.log(colaborador);
+            })
+            .catch(error => console.log('error', error));
     }
 
 
-    const eliminarCliente = (id) => {
-        let eliminar = window.confirm("Quieres eliminar el colaborador?");
-        console.log(eliminar);
-
-        if (eliminar === true) {
-            alert('Se ha eliminado el colaborador')
+    const eliminarEmpleado = (idUser, fullName) => {
+        Swal.fire({
+            title: 'Eliminar',
+            text: 'Quieres eliminar a ' + fullName + '?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar!',
+            cancelButtonText : 'Cancelar'
         }
+        ).then((result) => {
+            if (result.isConfirmed) {
+
+                var myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/json");
+
+                var raw = JSON.stringify({
+                    "id": idUser
+                });
+
+                var requestOptions = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    body: raw,
+                    redirect: 'follow'
+                };
+
+                fetch("http://localhost:3014/api/users/delete", requestOptions)
+                    .then(response => response.json())
+                    .then(result => {
+                        let message = result.message;
+
+                        if (message === 'OK') {
+                            Swal.fire({
+                                title: 'Eliminado',
+                                text: 'Se ha eliminado a ' + fullName,
+                                icon: 'success',
+                                confirmButtonText: 'OK'
+                            })
+                        }
+                        else {
+                            Swal.fire({
+                                title: 'Error',
+                                text: 'Ha ocurridon un error',
+                                icon: 'error',
+                                confirmButtonText: 'OK'
+                            })
+                        }
+                    })
+                    .catch(error => console.log('error', error));
+            } else if (result.isDenied) {
+                Swal.fire('Se ha cancelado', '', 'info')
+            }
+        })
 
     }
 
@@ -54,41 +118,41 @@ function Colaboradores() {
 
                 {
 
-                    clientes[0] ? 
-                    <div className='table-responsive-md'> 
-                        <table className="table my-5">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nombre Completo</th>
+                    colaboradores[0] ?
+                        <div className='table-responsive-md'>
+                            <table className="table my-5">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Nombre Completo</th>
 
-                                <th scope="col">Rol</th>
-                                <th scope="col">Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {clientes.map((clients) => (
-                                sumarContador(),
+                                        <th scope="col">Rol</th>
+                                        <th scope="col">Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {colaboradores.map((items) => (
+                                        sumarContador(),
 
-                                <tr key={clients._id}>
+                                        <tr key={items._id}>
 
-                                    <th scope="row">{contador}</th>
-                                    <td><Link  id='title-table' to={'/dashboard/colaboradores/detallecolaborador'}>{clients.nombreCompleto}</Link></td>
-                                    <td>
-                                        {clients.rol}
-                                    </td>
-                                    <td className='acciones'>
-                                        <Link to={'/dashboard/colaboradores/editar'} className="btn btn-success">Edit</Link>
-                                        <Link to={'/dashboard'} onClick={() => eliminarCliente(clients.id)} className="btn btn-danger">Delete</Link>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                                            <th scope="row">{contador}</th>
+                                            <td><Link id='title-table' to={'/dashboard/colaboradores/detallecolaborador'}>{items.fullName}</Link></td>
+                                            <td>
+                                                {items.role}
+                                            </td>
+                                            <td className='acciones'>
+                                                <Link to={'/dashboard/colaboradores/editar'} className="btn btn-success">Edit</Link>
+                                                <Link to={'/dashboard/colaboradores'} onClick={() => eliminarEmpleado(items._id, items.fullName)} className="btn btn-danger">Delete</Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
 
-                    <hr />
-                    </div>
-                     : <div className='loading'> <h4>Loading...</h4> </div>
+                            <hr />
+                        </div>
+                        : <div className='loading'> <h4>Loading...</h4> </div>
                 }
             </div>
 
